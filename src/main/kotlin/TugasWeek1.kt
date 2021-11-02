2889151cf5886f35d75e1235505b0f0c18d6ec0b
itsmearss <annurriyadhus17@gmail.com>
2021-11-02 21:01:50 +0700

    first commit

---
+++ src/main/kotlin/TugasWeek1.kt
@@ -0,0 +1,12 @@
+fun main() {
+    var Annur = """
+        Annur Riyadhus Solikhin
+        21090096
+        Brebes, 27 Desember 2002
+        1C
+        Teknik Informatika
+    """.trimIndent()
+
+    println(Annur)
+    println("Terdapat ${Annur.length} huruf")
+}
